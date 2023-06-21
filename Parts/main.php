5e6b78615192bdcfd605d28a8832a385fc5dab7d
RafixOOO <smoktbg@gmail.com>
2023-06-21 10:49:22 +0200

    commit

---
+++ Parts/main.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Strona główna</title>
+    <meta charset ="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+<table class="table table-responsive table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Zespół</th>
+      <th scope="col">Pozycja</th>
+      <th scope="col">Ilosc</th>
+      <th scope="col">Profil</th>
+      <th scope="col">Materiał</th>
+      <th scope="col">Długość</th>
+      <th scope="col">Ciężar</th>
+      <th scope="col">Całkowity Ciężar</th>
+      <th scope="col">Data</th>
+      <th scope="col">Opcje</th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr data-bs-toggle="collapse" data-bs-target="#rozwijanyWiersz1" aria-expanded="false" aria-controls="rozwijanyWiersz1">
+        <td class="glyphicon glyphicon-plus"></td>
+        <td>1</td>
+        <td>1</td>
+        
+
+    </tr>
+    <tr id="rozwijanyWiersz1" class="collapse">
+        <td>ahdas</td>
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</body>
+</html>
