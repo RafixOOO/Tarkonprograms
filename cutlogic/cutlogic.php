6c654e7e9c94aeb7a8d1ea4370c6d17398dc7f48
Rafał Pezda <smoktbg@gmail.com>
2023-11-02 12:26:43 +0100

    2.1

--- cutlogic/cutlogic.php
+++ cutlogic/cutlogic.php
@@ -2,7 +2,8 @@
 require_once("dbconnect.php");
 
 $sqlcut = "SELECT ID, PROGRAM, PROJEKT, OPIS, DLUGOSC, RESZTKI, CZESC, CZDLU, CNT, checkpr
-FROM PartCheck.dbo.cutlogic;";
+FROM PartCheck.dbo.cutlogic
+Order by PROJEKT Desc";
 $datacut = sqlsrv_query($conn, $sqlcut); 
 
 ?>
