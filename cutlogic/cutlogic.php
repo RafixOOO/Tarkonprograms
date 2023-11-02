a0de6f840bae4c9ef194702c302edf885b91c48e
Rafał Pezda <smoktbg@gmail.com>
2023-11-02 13:48:01 +0100

    2.1

--- cutlogic/cutlogic.php
+++ cutlogic/cutlogic.php
@@ -3,7 +3,7 @@ require_once("dbconnect.php");
 
 $sqlcut = "SELECT ID, PROGRAM, PROJEKT, OPIS, DLUGOSC, RESZTKI, CZESC, CZDLU, CNT, checkpr
 FROM PartCheck.dbo.cutlogic
-Order by PROJEKT Desc";
+Order by checkpr asc";
 $datacut = sqlsrv_query($conn, $sqlcut); 
 
 ?>
--- cutlogic/main.php
+++ cutlogic/main.php
@@ -3,6 +3,11 @@
 <head>
     <?php include 'globalhead.php'; 
     require_once('../auth.php');?>
+    <style>
+      tr.selected {
+    background-color: #cce5ff; /* Kolor tła dla zaznaczonych wierszy */
+}
+      </style>
 </head>
 <body class="p-3 mb-2 bg-light bg-gradient text-dark" id="error-container">
 <?php include 'globalnav.php'; ?>
@@ -23,12 +28,13 @@ require_once 'cutlogic.php';
     <th scope="col">Część</th>
     <th scope="col">Długość Części</th>
     <th scope="col">Liczba</th>
+    <th scope="col">Opcje</th>
   </tr>
 </thead>
 <tbody>
 <?php while ($row = sqlsrv_fetch_array($datacut, SQLSRV_FETCH_ASSOC)) {
 ?>  
-<tr>
+<tr value="<?php echo $row['ID']; ?>">
 <td><?php echo $row['PROGRAM']; ?></td>
 <td><?php echo $row['PROJEKT']; ?></td>
 <td><?php echo $row['OPIS']; ?></td>
@@ -37,9 +43,36 @@ require_once 'cutlogic.php';
 <td><?php echo $row['CZESC']; ?></td>
 <td><?php echo $row['CZDLU']; ?></td>
 <td><?php echo $row['CNT']; ?></td>
+<td><?php if($row['checkpr'] == 1) { ?><Button class='btn btn-success btn-sm' disabled>Zakończono</Button><?php } else { ?><?php if(isUserCutlogic()) { ?><Button class='btn btn-primary btn-sm'>Zakończ</Button><?php } ?><?php } ?></td>
 </tr>
   <?php } ?>
 </tbody>
 </table>
 </body>
+<script src="../static/jquery-3.7.0.js"></script>
+<script src="../static/jquery.dataTables.min.js"></script>
+<script src="../static/dataTables.bootstrap5.min.js"></script>
+<script>
+
+    $(document).ready(function() {
+    $('.btn-primary').on('click', function() {
+        var button = $(this);
+        var rowId = button.closest('tr').attr('value'); // Pobierz ID z atrybutu 'value' rodzica przycisku
+
+        // Wywołaj AJAX, aby zaktualizować dane w bazie danych
+        $.ajax({
+            url: 'updatecut.php', // Ścieżka do pliku PHP, który obsłuży aktualizację bazy danych
+            method: 'POST',
+            data: { rowId: rowId }, // Przesyłanie ID wiersza do serwera
+            success: function(response) {
+                console.log(response); // Wyświetl odpowiedź z serwera w konsoli przeglądarki
+                button.text('Zakończono'); // Zmień tekst przycisku na "Zakończono"
+                button.removeClass('btn-primary').addClass('btn-success'); // Zmień styl przycisku na zielony
+                button.prop('disabled', true); // Zablokuj przycisk, aby uniknąć kolejnych kliknięć
+            }
+        });
+    });
+    $('#myTable').DataTable();
+});
+</script>
 </html>
