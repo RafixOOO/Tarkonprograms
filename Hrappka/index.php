c78098281db7e7ef17a7a321efcfff0ccdd68ed2
rafal.pezda <smoktbg@gmail.com>
2024-02-16 10:34:34 +0100

    2.5

--- Hrappka/index.php
+++ Hrappka/index.php
@@ -10,7 +10,7 @@ require_once('../auth.php');
   <body id="colorbox" class="p-3 mb-2 bg-light bg-gradient text-dark" id="error-container">
   <?php require_once('globalnav.php'); ?>
   <div class="container-fluid">
-  <iframe src="https://hrappka.budhrd.eu/work-time-register?widget_hash=488a3e4adca6545878db8ec4163c15fd" width="100%" height="70%"></iframe>
+  <iframe src="https://hrappka.budhrd.eu/work-time-register?widget_hash=488a3e4adca6545878db8ec4163c15fd" width="100%" height="100%"></iframe>
   </div>
 </body>
 </html>
