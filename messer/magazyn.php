f913ef624b7e9493ee42b34a9d895de374162cdd
rafal.pezda <smoktbg@gmail.com>
2024-02-29 15:49:47 +0100

    2.5

--- messer/magazyn.php
+++ messer/magazyn.php
@@ -1,13 +1,13 @@
 <?php
 require_once("dbconnect.php");
 
-$sql="Select
+$sql="SELECT
     m.PartID,
     MAX(m.[Date]) AS data,
     m.Person,
     m.Localization,
-    COUNT(m.PartID) AS Ilosc,
-    COUNT(sh1.SheetName) as zuzyte,
+    (SELECT COUNT(l.PartID) from PartCheck.dbo.MagazynExtra l where l.PartID=m.PartID and l.Localization=m.Localization) AS Ilosc,
+    (SELECT COUNT(h.SheetName) from SNDBASE_PROD.dbo.StockArchive h where h.SheetName=sh1.SheetName) as zuzyte,
     s.Material,
     s.Thickness,
     s.[Length],
@@ -17,7 +17,7 @@ FROM
 LEFT JOIN
     SNDBASE_PROD.dbo.Stock s ON m.PartID = s.SheetName COLLATE SQL_Latin1_General_CP1_CI_AS
 LEFT JOIN
-   	SNDBASE_PROD.dbo.StockArchive sh1 on m.PartID=sh1.SheetName COLLATE SQL_Latin1_General_CP1_CI_AS
+    SNDBASE_PROD.dbo.StockArchive sh1 on m.PartID=sh1.SheetName COLLATE SQL_Latin1_General_CP1_CI_AS
 WHERE NOT EXISTS (
         SELECT 1
         FROM
@@ -27,7 +27,7 @@ WHERE NOT EXISTS (
             and sh.Qty=0
     ) and Deleted = 0
 GROUP BY
-    m.PartID, m.Person, m.Localization, s.Material, s.Thickness, s.[Length], s.Width
+    m.PartID, m.Person, m.Localization, s.Material, s.Thickness, s.[Length], s.Width, sh1.SheetName
 ORDER BY
     MAX(m.[Date]) DESC;";
  $datas = sqlsrv_query($conn, $sql);
