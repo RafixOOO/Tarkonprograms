7d54a6c433fb500465efc8d27152dd17b0272302
rafal.pezda <smoktbg@gmail.com>
2024-05-23 15:41:22 +0200

    2.5

--- messer/magazynarch.php
+++ messer/magazynarch.php
@@ -17,14 +17,12 @@ $sql="SELECT
     m.Localization,
     (SELECT COUNT(l.PartID) from PartCheck.dbo.MagazynExtra l where l.PartID=m.PartID and l.Localization=m.Localization ) AS Ilosc,
     (SELECT COUNT(h.SheetName) from SNDBASE_PROD.dbo.StockArchive h where h.SheetName=sh1.SheetName) as zuzyte,
-    s.Material,
-    s.Thickness,
-    s.[Length],
-    s.Width
+    sh1.Material,
+    sh1.Thickness,
+    sh1.[Length],
+    sh1.Width
 FROM
     PartCheck.dbo.MagazynExtra m
-LEFT JOIN
-    SNDBASE_PROD.dbo.Stock s ON m.PartID = s.SheetName COLLATE SQL_Latin1_General_CP1_CI_AS
 LEFT JOIN
     SNDBASE_PROD.dbo.StockArchive sh1 on m.PartID=sh1.SheetName COLLATE SQL_Latin1_General_CP1_CI_AS
 WHERE m.Deleted = 1 or EXISTS (
@@ -36,7 +34,10 @@ WHERE m.Deleted = 1 or EXISTS (
         AND sh1.Qty = 0
     )
 GROUP BY
-    m.PartID, m.Localization, s.Material, s.Thickness, s.[Length], s.Width, sh1.SheetName, m.Deleted
+    m.PartID, m.Localization, sh1.Material,
+    sh1.Thickness,
+    sh1.[Length],
+    sh1.Width, sh1.SheetName, m.Deleted
 ORDER BY
     MAX(m.[Date]) DESC;";
 $datas = sqlsrv_query($conn, $sql);
