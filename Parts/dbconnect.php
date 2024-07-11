7dd584cb21004621fe5e91ef9088d5cd473d0d8c
rafal.pezda <smoktbg@gmail.com>
2024-07-11 11:03:28 +0200

    3.0

--- Parts/dbconnect.php
+++ Parts/dbconnect.php
@@ -4,7 +4,8 @@ $serverName = '10.100.100.48,49827';
 $connectionOptions = array(
     "Database" => "PartCheck",
     "Uid" => "Sa",
-    "PWD" => "Shark1445NE\$T"
+    "PWD" => "Shark1445NE\$T",
+    "CharacterSet" => "UTF-8"
 );
 
 $conn = sqlsrv_connect($serverName, $connectionOptions);
--- dbconnect.php
+++ dbconnect.php
@@ -3,8 +3,7 @@ $serverName = '10.100.100.48,49827';
 $connectionOptions = array(
     "Database" => "PartCheck",
     "Uid" => "Sa",
-    "PWD" => "Shark1445NE\$T",
-    "CharacterSet" => "UTF-8"
+    "PWD" => "Shark1445NE\$T"
 );
 
 $conn = sqlsrv_connect($serverName, $connectionOptions);
