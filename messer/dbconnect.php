21ac1c60ac1ca96740031cc7ba39892f069e07ff
rafal.pezda <smoktbg@gmail.com>
2024-07-18 15:36:00 +0200

    3.0

--- messer/dbconnect.php
+++ messer/dbconnect.php
@@ -6,7 +6,8 @@ $serverName = '10.100.100.48,49827';
 $connectionOptions = array(
     "Database" => "PartCheck",
     "Uid" => "Sa",
-    "PWD" => "Shark1445NE\$T"
+    "PWD" => "Shark1445NE\$T",
+    "CharacterSet" => "UTF-8"
 );
 
 $conn = sqlsrv_connect($serverName, $connectionOptions);
