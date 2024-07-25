47c82f25ba23fa822755ff83ec5458b5580da3ec
rafal.pezda <smoktbg@gmail.com>
2024-07-25 15:00:16 +0200

    3.0

--- Parts/main.php
+++ Parts/main.php
@@ -178,6 +178,8 @@ FROM PartCheck.dbo.hrappka_godziny where cr_number='$row1[cr_number]';";
 
     </div>
     <?php if (!isLoggedIn()) { ?>
+        <link rel="stylesheet" href="../assets/css/plugins.min.css" />
+        <link rel="stylesheet" href="../assets/css/kaiadmin.min.css" />
         <script src="../assets/js/plugin/webfont/webfont.min.js"></script>
         <script src="../assets/js/core/jquery-3.7.1.min.js"></script>
         <script src="../assets/js/core/popper.min.js"></script>
